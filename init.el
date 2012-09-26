3d75521da862d01f7d27c180ea27d24ae17b7d3d
Jaseem Abid <jaseemabid@gmail.com>
2012-09-26 21:21:57 +0530

    Emacs jargon

--- init.el
+++ init.el
@@ -29,6 +29,10 @@
  '(tool-bar-mode nil)
  '(transient-mark-mode t))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:family "Droid Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
 
 ;; Custom packages
