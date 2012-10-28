51bc71a367a3bbc20576c8f600cf487e516cedd0
Jaseem Abid <jaseemabid@gmail.com>
2012-10-28 16:14:42 +0530

    zip and unzip in dired-mode

--- init.el
+++ init.el
@@ -148,6 +148,41 @@
 (add-hook 'text-mode-hook 'auto-fill-mode)
 (add-hook 'text-mode-hook 'flyspell-mode)
 
+;; Zip well in dired mode
+
+(eval-after-load "dired-aux"
+  '(add-to-list 'dired-compress-file-suffixes
+				'("\\.zip\\'" ".zip" "unzip")))
+
+(eval-after-load "dired"
+  '(define-key dired-mode-map "z" 'dired-zip-files))
+
+(defun dired-zip-files (zip-file)
+  "Create an archive containing the marked files."
+  (interactive "sEnter name of zip file: ")
+
+  ;; create the zip file
+  (let ((zip-file (if (string-match ".zip$" zip-file) zip-file (concat zip-file ".zip"))))
+    (shell-command
+     (concat "zip "
+             zip-file
+             " "
+             (concat-string-list
+              (mapcar
+               '(lambda (filename)
+                  (file-name-nondirectory filename))
+               (dired-get-marked-files))))))
+  (revert-buffer)
+  ;; remove the mark on all the files  "*" to " "
+  ;; (dired-change-marks 42 ?\040)
+  ;; mark zip file
+  ;; (dired-mark-files-regexp (filename-to-regexp zip-file))
+  )
+
+(defun concat-string-list (list)
+  "Return a string which is a concatenation of all elements of the list separated by spaces"
+  (mapconcat '(lambda (obj) (format "%s" obj)) list " "))
+
 ;; Handle extra filetypes
 
 ;; PKGBUILKD mode
