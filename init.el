1f5328509561957fedef476021f1104297493f3c
Jaseem Abid <jaseemabid@gmail.com>
2013-03-22 04:06:12 +0530

    Inconsolata it is :)

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Droid Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Inconsolata")))))
 
 ;; Start maximized
 (set-frame-parameter nil 'fullscreen 'maximized)
