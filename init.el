6fdf3bbf0a64ffd5f3c7279697905368fefd75f6
Jaseem Abid <jaseemabid@gmail.com>
2013-03-21 23:29:42 +0530

    Real programmers use the real lambda

--- init.el
+++ init.el
@@ -240,6 +240,27 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (load-theme 'solarized-dark t)
 
+;; Real programmers use the real lambda
+(defun sm-lambda-mode-hook ()
+  (font-lock-add-keywords
+   nil `(("\\<lambda\\>"
+   (0 (progn (compose-region (match-beginning 0) (match-end 0)
+        ,(make-char 'greek-iso8859-7 107))
+      nil)))))
+
+  (font-lock-add-keywords
+   nil `(("\\<function\\>"
+   (0 (progn (compose-region (match-beginning 0) (match-end 0)
+        ,(make-char 'greek-iso8859-7 107))
+      nil)))))
+  )
+
+(add-hook 'emacs-lisp-mode-hook 'sm-lambda-mode-hook)
+(add-hook 'lisp-interactive-mode-hook 'sm-lamba-mode-hook)
+(add-hook 'scheme-mode-hook 'sm-lambda-mode-hook)
+
+(add-hook 'js2-mode-hook 'sm-lambda-mode-hook)
+
 ;; Load extra packages installed with elpa
 (add-hook 'after-init-hook
 		  '(lambda ()
