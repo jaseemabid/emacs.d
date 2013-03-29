02f67f58891bc84dceae9eb15f48b8df85154309
Jaseem Abid <jaseemabid@gmail.com>
2013-03-29 16:17:57 +0530

    Some haskell mode stuff

--- after-init.el
+++ after-init.el
@@ -35,3 +35,13 @@
 ;; rinari
 (require 'rinari)
 (global-rinari-mode)
+
+;; Haskell mode stuff
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;; hslint on the command line only likes this indentation mode;
+;; alternatives commented out below.
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;; Ignore compiled Haskell files in filename completions
+(add-to-list 'completion-ignored-extensions ".hi")
