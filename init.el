8ec9c659ddc5e85b215f54b0df2c8a5c6fadd0bd
Jaseem Abid <jaseemabid@gmail.com>
2013-04-09 11:12:32 +0530

    Remove random crap

--- init.el
+++ init.el
@@ -144,15 +144,6 @@
 ;; ido mode :)
 (ido-mode t)
 
-;; openwith-mode
-'(openwith-associations (quote (("\\.pdf\\'" "evince" (file))
-								("\\.mp3\\'" "clementine" (file))
-								("\\.\\(?:mpe?g\\|avi\\|wmv\\)\\'" "vlc"
-								 ("-idx" file)) ("" "" nil)
-								 ("\\.\\(?:jp?g\\|png\\|JPG\\)\\'" "eog" (file)))
-							   )
-						)
-(add-hook 'dired-mode-hook 'openwith-mode)
 
 ;; ;; Hooks
 
@@ -182,40 +173,6 @@
 
 ;; Guru-mode to do it the right way
 ;; (add-hook 'prog-mode-hook 'turn-on-guru-mode)
-
-;; Zip well in dired mode
-(eval-after-load "dired-aux"
-  '(add-to-list 'dired-compress-file-suffixes
-				'("\\.zip\\'" ".zip" "unzip")))
-
-(eval-after-load "dired"
-  '(define-key dired-mode-map "z" 'dired-zip-files))
-
-(defun dired-zip-files (zip-file)
-  "Create an archive containing the marked files."
-  (interactive "Enter name of zip file: ")
-
-  ;; create the zip file
-  (let ((zip-file (if (string-match ".zip$" zip-file) zip-file (concat zip-file ".zip"))))
-    (shell-command
-     (concat "zip "
-             zip-file
-             " "
-             (concat-string-list
-              (mapcar
-               '(lambda (filename)
-                  (file-name-nondirectory filename))
-               (dired-get-marked-files))))))
-  (revert-buffer)
-  ;; remove the mark on all the files  "*" to " "
-  ;; (dired-change-marks 42 ?\040)
-  ;; mark zip file
-  ;; (dired-mark-files-regexp (filename-to-regexp zip-file))
-  )
-
-(defun concat-string-list (list)
-  "Return a string which is a concatenation of all elements of the list separated by spaces"
-  (mapconcat '(lambda (obj) (format "%s" obj)) list " "))
 
 ;; Handle extra filetypes
 
