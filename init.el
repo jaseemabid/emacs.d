069f6100e9239d1c08849efa4f4865aff27ed230
Jaseem Abid <jaseemabid@gmail.com>
2013-09-08 00:36:29 +0530

    Jump to specific lines if mentioned

--- init.el
+++ init.el
@@ -176,8 +176,35 @@
 (global-set-key (kbd "C-c f") 'flyspell-buffer)
 
 ;; eshell tweaks
+
+;; Open files and go places like we see from error messages, i e: path:line:col
+;; (to-do "make `find-file-line-number' work for emacsclient as well")
+;; (to-do "make `find-file-line-number' check if the file exists")
+(defadvice find-file (around find-file-line-number
+                             (path &optional wildcards)
+                             activate)
+  "Turn files like file.js:14:10 into file.js and going to line 14, col 10."
+  (save-match-data
+    (let* ((match (string-match "^\\(.*?\\):\\([0-9]+\\):?\\([0-9]*\\)$" path))
+           (line-no (and match
+                         (match-string 2 path)
+                         (string-to-number (match-string 2 path))))
+           (col-no (and match
+                        (match-string 3 path)
+                        (string-to-number (match-string 3 path))))
+           (path (if match (match-string 1 path) path)))
+      ad-do-it
+      (when line-no
+        ;; goto-line is for interactive use
+        (goto-char (point-min))
+        (forward-line (1- line-no))
+        (when (> col-no 0)
+          (forward-char (1- col-no)))))))
+
 (defalias 'open 'find-file)
+(defalias 'o 'find-file)
 (defalias 'openo 'find-file-other-window)
+
 (defun eshell/clear ()
   "clear shell buffer"
   (interactive)
