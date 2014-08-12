c5eb4ad22b65c1bd9e6d33aaffedf52bfc77d254
Jaseem Abid <jaseemabid@gmail.com>
2014-08-13 02:08:20 +0530

    Fix solarized config vars load order

--- angel-dark-theme.el
+++ angel-dark-theme.el
@@ -1,25 +1,26 @@
 (deftheme angel-dark
   "A dark theme based on solarized dark.")
 
-;; [todo] - Derive angel-dark from solarized
-;; [todo] - Disable solarized with angel-dark
-(load-theme 'solarized-dark t)
-
 (custom-theme-set-variables
  'angel-dark
  '(magit-use-overlays t)
  '(fci-rule-width 1)
- '(cursor-type (quote (bar . 2)))
- '(solarized-height-plus-1 1)
- '(solarized-height-plus-2 1)
- '(solarized-height-plus-3 1)
- '(solarized-height-plus-4 1)
- '(solarized-use-less-bold t)
- '(solarized-use-more-italic t)
- '(solarized-use-variable-pitch nil))
+ '(cursor-type (quote (bar . 2))))
 
 (custom-theme-set-faces
  'angel-dark
  '(magit-item-highdark ((t nil))))
+
+(setq solarized-height-plus-1 1.1
+      solarized-height-plus-2 1.1
+      solarized-height-plus-3 1.1
+      solarized-height-plus-4 1.1
+      solarized-use-less-bold t
+      solarized-use-more-italic t
+      solarized-use-variable-pitch nil)
+
+;; [todo] - Derive angel-dark from solarized
+;; [todo] - Disable solarized with angel-dark
+(load-theme 'solarized-dark t)
 
 (provide-theme 'angel-dark)
