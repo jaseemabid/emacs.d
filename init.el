7d556181e99c3cb619bdb83d13f6040908aad4b3
Jaseem Abid <jaseemabid@gmail.com>
2014-11-14 02:20:48 +0530

    Set fci-mode ❌ by default

--- init.el
+++ init.el
@@ -490,7 +490,7 @@
 
 ;; prog-mode
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
-(add-hook 'prog-mode-hook 'fci-mode)
+;; (add-hook 'prog-mode-hook 'fci-mode)
 
 ;; Rectangle mode
 (defalias 'rectangle-insert-string 'string-insert-rectangle
