9b3eaaebb7f9e9a1d0a3e9cf6ef6db90f818d9ce
Jaseem Abid <jaseemabid@gmail.com>
2015-02-18 15:00:53 +0530

    Add sort-words

--- snippets.el
+++ snippets.el
@@ -76,3 +76,14 @@ point reaches the beginning or end of the buffer, stop there."
 ;; remap C-a to `smarter-move-beginning-of-line'
 (global-set-key [remap move-beginning-of-line]
                 'smarter-move-beginning-of-line)
+
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+Prefixed with negative \\[universal-argument], sorts in reverse.
+
+The variable `sort-fold-case' determines whether alphabetic case
+affects the sort order.
+
+See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
