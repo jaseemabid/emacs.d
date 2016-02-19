9547425a6404919ce7abd3f723daffb6f1d8634e
Jaseem Abid <jaseemabid@gmail.com>
2016-02-19 16:06:32 +0530

    Remove eshell-here, which I never used

--- eshell/init.el
+++ eshell/init.el
@@ -29,24 +29,6 @@
         (when (> col-no 0)
           (forward-char (1- col-no)))))))
 
-(defun eshell-here ()
-  "Opens up a new shell in the directory associated with the
-current buffer's file. The eshell is renamed to match that
-directory to make multiple eshell windows easier."
-  (interactive)
-  (let* ((parent (if (buffer-file-name)
-                     (file-name-directory (buffer-file-name))
-                   default-directory))
-         (height (/ (window-total-height) 3))
-         (name   (car (last (split-string parent "/" t)))))
-    (split-window-vertically (- height))
-    (other-window 1)
-    (eshell "new")
-    (rename-buffer (concat "*eshell: " name "*"))
-
-    (insert (concat "ls"))
-    (eshell-send-input)))
-
 (defun eshell/x ()
   (insert "exit")
   (eshell-send-input)
@@ -67,6 +49,5 @@ directory to make multiple eshell windows easier."
             (local-set-key (kbd "C-z") 'bury-buffer)))
 
 (global-set-key (kbd "C-z") 'eshell)
-(global-set-key (kbd "C-!") 'eshell-here)
 
 (setq eshell-buffer-maximum-lines 4096)
